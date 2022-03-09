4da3aa75ef082bcf44a20e57e64a50557fd881f1
tuantu499 <tuantu499@gmail.com>
2022-03-09 10:48:07 +0700

    i can't use xpath to do

---
+++ app.py
@@ -0,0 +1,31 @@
+from distutils import extension
+from email.policy import default
+from lxml import html
+import re
+import json
+import csv
+import click
+from itertools import chain
+import re
+
+
+file = open("men shoes.txt")
+line = file.read().replace("\n", " ")
+file.close()
+tree = html.fromstring(html=line)
+all_product = tree.xpath("//div[@class = 'cell']/ul")[0]
+title = all_product.xpath(".//li/div/div[@class ='productDetail']/div/a/text()")
+price = all_product.xpath(".//li/div/div[@class ='productDetail']/div/div/div/div/span/text()")
+res1 = []
+res2 = []
+
+for ele in title:
+    if ele.strip():
+        res1.append(ele)
+ree1 =[''.join(item.split()) for item in res1]
+for ele in price:
+    if ele.strip():
+        res2.append(ele)
+ree2 =[''.join(item.split()) for item in res2]
+print(len(ree1))
+print(len(ree2))
